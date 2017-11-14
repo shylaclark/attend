b747f315fc8cb207b6108eff855ded3451a2357f
Shyla Clark <shylaclark@gmail.com>
2017-11-14 15:12:34 -0600

    Created screen test classes with snapshot test

---
+++ AttendApp/__tests__/HomeScreen-test.js
@@ -0,0 +1,14 @@
+// __tests__/Intro-test.js
+import 'react-native';
+import React from 'react';
+import HomeScreen from '../HomeScreen.js';
+
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <HomeScreen />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
