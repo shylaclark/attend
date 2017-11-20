4bb4458d88b7a8f582dc8dafd3d15c7e3788ae21
Shyla Clark <shylaclark@gmail.com>
2017-11-20 13:49:25 -0600

    Added working snapshot test of App.js

---
+++ AttendApp/__tests__/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'react-native';
+import renderer from 'react-test-renderer';
+import App from '../src/App';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <App />
+  );
+});
