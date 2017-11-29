d5d2d5e425051971fef03569ba34a853bcc75f34
psathvik1118 <spingili@uno.edu>
2017-11-29 07:42:55 -0600

    tests for all screens passed as per the requirement

---
+++ AttendApp/CourseList.js
@@ -0,0 +1,248 @@
+import React, {Component} from 'react';
+
+import {AppRegistry, Text, View, Image, ListView, StyleSheet, TouchableOpacity} from 'react-native';
+import AttendanceSheet from "./AttendanceSheet";
+import RowLink from "../components/RowLink";
+import CreateCourse from "./CreateCourse";
+import Header from "../components/Header";
+import ListFooter from "../components/ListFooter";
+import SectionHeader from "../components/SectionHeader";
+
+const courses = [
+    {name: 'CSCI 5101- Programming Language Structure', instructor: 'Dr. ThisGuy'},
+    {name: 'CSCI 6363- Agile Software Engineering', instructor: 'Dr. ThisGuy'},
+    {name: 'CSCI 5501- Analysis of Algorithms', instructor: 'Dr. ThisGuy'},
+    {name: 'CSCI 3301- Computer Organization', instructor: 'Dr. ThisGuy'},
+    {name: 'CSCI 2467- Systems Programming Concepts', instructor: 'Dr. ThisGuy'}
+]
+const background = require("../img/background.png");
+
+export default class CourseList extends Component{
+    constructor(props) {
+        super(props);
+
+        const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId];
+        const getRowData = (dataBlob, sectionId, rowId) => dataBlob[`${rowId}`];
+
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2,
+            sectionHeaderHasChanged : (s1, s2) => s1 !== s2,
+            getSectionData,
+            getRowData,
+    });
+
+        const { dataBlob, sectionIds, rowIds } = this.formatData(courses);
+        this.state = {
+            courseDataSource: ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds),
+        };
+    }
+
+    renderRow(course, sectionId, rowId, highlightRow){
+        return(
+            <RowLink navigation={this.props.navigation} title={course.name}/></RowLink>
+    )
+    }
+
+    formatData(data) {
+        /* We're sorting by alphabetically so we need the alphabet*/
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+        /* Need somewhere to store our data*/
+        const dataBlob = {};
+        const sectionIds = [];
+        const rowIds = [];
+
+        /* Each section is going to represent a letter in the alphabet so we loop over the alphabet*/
+        for (let sectionId = 0; sectionId < alphabet.length; sectionId++) {
+            /* Get the character we're currently looking for*/
+            const currentChar = alphabet[sectionId];
+
+            /* Get course names that start with the current letter*/
+            const course = data.filter((course) => course.name.toUpperCase().indexOf(currentChar) === 0);
+
+            /* If there are any courses that have a name starting with the current letter then we'll
+             * add a new section otherwise we just skip over it
+             */
+            if (course.length > 0) {
+                /* Add a section id to our array so the listview knows that we've got a new section*/
+                sectionIds.push(sectionId);
+
+                /* Store any data we would want to display in the section header. In our case we want to show
+                 * the current character
+                 */
+                dataBlob[sectionId] = { character: currentChar };
+
+                /* Setup a new array that we can store the row ids for this section*/
+                rowIds.push([]);
+
+                /* Loop over the valid classes for this section*/
+                for (let i = 0; i < course.length; i++) {
+                    /* Create a unique row id for the data blob that the listview can use for reference*/
+                    const rowId = `${sectionId}:${i}`;
+
+                    /* Push the row id to the row ids array. This is what listview will reference to pull
+                     * data from our data blob
+                     */
+                    rowIds[rowIds.length - 1].push(rowId);
+
+                    /* Store the data we care about for this row*/
+                    dataBlob[rowId] = course[i];
+                }
+            }
+        }
+
+        return { dataBlob, sectionIds, rowIds };
+    }
+
+    render(){
+        const {navigate} = this.props.navigation;
+        return(
+            <Image source={background} style={styles.background} resizeMode="cover">
+
+            <ListView
+        style={styles.container}
+        enableEmptySections={false}
+        automaticallyAdjustContentInsets={false}
+        dataSource={this.state.courseDataSource}
+        renderRow={this.renderRow.bind(this)}
+        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+        renderHeader={() => <Header title={'Courses'} />}
+        renderFooter={() => <ListFooter title = {'Create New Course}
+            renderSectionHeader={(sectionData) => <SectionHeader {...sectionData} />}
+        contentBackgroundColor={'black'}
+
+        /></ListView>
+
+        </Image>
+    );
+    }
+}
+
+const styles = StyleSheet.create({
+    row: {
+        flex:1,
+        justifyContent:'center',
+        padding:10,
+        backgroundColor: 'black',
+        marginBottom:3
+    },
+    rowText: {
+        flex:1,
+        flexDirection: 'row',
+        color: 'white'
+    },
+    separator: {
+
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: '#8E8E8E',
+    },
+    container: {
+        flex:1,
+        flexDirection:'column'
+    },
+    bg: {
+        paddingTop: 30,
+        width: null,
+        height: null,
+    },
+    background: {
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',           // Center vertically
+        alignItems: 'center',
+    },
+    headerContainer: {
+        flex: 3,
+    },
+    inputsContainer: {
+        marginTop: 100,
+    },
+    footerContainer: {
+        flex: 1
+    },
+    headerIconView: {
+        marginLeft: 10,
+        backgroundColor: 'transparent'
+    },
+    headerBackButtonView: {
+        width: 20,
+        height: 20,
+    },
+    backButtonIcon: {
+        width: 25,
+        height: 25,
+    },
+    headerTitleView: {
+        backgroundColor: 'transparent',
+        marginTop: 20,
+        marginLeft: 20,
+    },
+    titleViewText: {
+        fontSize: 40,
+        color: '#fff',
+    },
+    dropdownText: {
+        fontSize: 17,
+        color: '#fff',
+    },
+    inputs: {
+        paddingVertical: 20,
+    },
+    inputContainer: {
+        borderWidth: 1,
+        borderBottomColor: '#CCC',
+        borderColor: 'transparent',
+        flexDirection: 'row',
+        height: 50,
+    },
+    button: {
+        backgroundColor: "#FFF",
+        paddingVertical: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 30,
+    },
+    buttonText: {
+        color: "#000",
+        fontSize: 18,
+    },
+
+    iconContainer: {
+        paddingHorizontal: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputIcon: {
+        width: 20,
+        height: 20,
+    },
+    input: {
+        flex: 1,
+        paddingHorizontal: 10,
+    },
+    signup: {
+        backgroundColor: '#FFF',
+        paddingVertical: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    login: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+    },
+    greyFont: {
+        color: '#D8D8D8'
+    },
+    blackFont: {
+        color: '#000'
+    },
+    whiteFont: {
+        color: '#FFF'
+    }
+});
+
+
+AppRegistry.registerComponent('CourseList', () => CourseList);
---
+++ AttendApp/SignupScreen.js
@@ -0,0 +1,323 @@
+import React, { Component } from 'react'
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    Picker,
+    TouchableWithoutFeedback
+} from 'react-native'
+
+import ModalDropdown from 'react-native-modal-dropdown';
+
+const background = require("../img/background.png");
+const backIcon = require("../img/back.png");
+const personIcon = require("../img/person.png");
+const lockIcon = require("../img/lock.png");
+const emailIcon = require("../img/email.png");
+const wifiIcon = require("../img/mac_address.png");
+
+const Realm = require('realm');
+
+export default class SignupScreen extends Component {
+
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        macAddress: '',
+        confirmPassword: ''
+    }
+
+    updateFormField = fieldName => text => {
+    this.setState({ [fieldName]: text })
+}
+
+createAccount = (navigate) => {
+    const { firstName, lastName, email, macAddress, password, confirmPassword } = this.state
+    // console.log('firstName', firstName);
+    // console.log('lastName', lastName);
+    // console.log('email', email);
+    // console.log('macAddress', macAddress);
+    // console.log('password', password);
+    // console.log('confirmPassword', confirmPassword);
+
+    Realm.open({
+        schema: [
+            {
+                name: 'User',
+                properties: {
+                    firstName: 'string',
+                    lastName: 'string',
+                    macAddress: 'string',
+                    email: 'string',
+                    password: 'string'
+                }
+            }
+        ]
+    }).then(realm => {
+
+        let existingUser = realm.objects('User').some(function(user) {
+            return user.email === email;
+        });
+
+    if (existingUser) {
+        alert("This email has been used already. Please enter a different email address.");
+    } else {
+        realm.write(() => {
+            realm.create('User',
+            {
+                firstName: firstName,
+                lastName: lastName,
+                macAddress: macAddress,
+                email: email,
+                password: password
+            }
+        );
+    });
+
+        navigate('Login');
+    }
+});
+};
+
+render() {
+    const {navigate} = this.props.navigation;
+
+    return (
+        <View style={styles.container}>
+<Image
+    source={background}
+    style={[styles.container, styles.bg]}
+    resizeMode="cover"
+        >
+        <View style={styles.headerContainer}>
+<View style={styles.headerTitleView}>
+<Text style={styles.titleViewText}>Sign Up</Text>
+    </View>
+    </View>
+
+    <View style={styles.inputsContainer}>
+<View style={styles.inputContainer}>
+<View style={styles.iconContainer}>
+<Image
+    source={personIcon}
+    style={styles.inputIcon}
+    resizeMode="contain"
+        />
+        </View>
+        <TextInput
+    onChangeText={this.updateFormField('firstName')}
+    style={[styles.input, styles.whiteFont]}
+    placeholder="First Name"
+    placeholderTextColor="#FFF"
+    underlineColorAndroid='transparent'
+        />
+        </View>
+
+        <View style={styles.inputContainer}>
+<View style={styles.iconContainer}>
+<Image
+    source={personIcon}
+    style={styles.inputIcon}
+    resizeMode="contain"
+        />
+        </View>
+        <TextInput
+    onChangeText={this.updateFormField('lastName')}
+    style={[styles.input, styles.whiteFont]}
+    placeholder="Last Name"
+    placeholderTextColor="#FFF"
+    underlineColorAndroid='transparent'
+        />
+        </View>
+
+        <View style={styles.inputContainer}>
+<View style={styles.iconContainer}>
+<Image
+    source={personIcon}
+    style={styles.inputIcon}
+    resizeMode="contain"
+        />
+        </View>
+        <ModalDropdown
+    options={['Instructor',
+        </View>
+
+        <View style={styles.inputContainer}>
+<View style={styles.iconContainer}>
+<Image
+    source={emailIcon}
+    style={styles.inputIcon}
+    resizeMode="contain"
+        />
+        </View>
+        <TextInput
+    onChangeText={this.updateFormField('email')}
+    style={[styles.input, styles.whiteFont]}
+    placeholder="UNO Email"
+    placeholderTextColor="#FFF"
+        />
+        </View>
+
+        <View style={styles.inputContainer}>
+<View style={styles.iconContainer}>
+<Image
+    source={wifiIcon}
+    style={styles.inputIcon}
+    resizeMode="contain"
+        />
+        </View>
+        <TextInput
+    onChangeText={this.updateFormField('macAddress')}
+    style={[styles.input, styles.whiteFont]}
+    placeholder="Mac Address"
+    placeholderTextColor="#FFF"
+        />
+        </View>
+
+        <View style={styles.inputContainer}>
+<View style={styles.iconContainer}>
+<Image
+    source={lockIcon}
+    style={styles.inputIcon}
+    resizeMode="contain"
+        />
+        </View>
+        <TextInput
+    onChangeText={this.updateFormField('password')}
+    secureTextEntry={true}
+    style={[styles.input, styles.whiteFont]}
+    placeholder="Password"
+    placeholderTextColor="#FFF"
+        />
+        </View>
+        <View style={styles.inputContainer}>
+<View style={styles.iconContainer}>
+<Image
+    source={lockIcon}
+    style={styles.inputIcon}
+    resizeMode="contain"
+        />
+        </View>
+        <TextInput
+    onChangeText={this.updateFormField('confirmPassword')}
+    secureTextEntry={true}
+    style={[styles.input, styles.whiteFont]}
+    placeholder="Confirm"
+    placeholderTextColor="#FFF"
+        />
+        </View>
+
+        </View>
+
+        <View style={styles.footerContainer}>
+
+<TouchableOpacity onPress={ () => {this.createAccount(navigate);} }>
+<View style={styles.signup}>
+<Text style={styles.blackFont}>Sign Up</Text>
+    </View>
+    </TouchableOpacity>
+
+    <TouchableOpacity>
+    <View style={styles.login}>
+<Text style={styles.greyFont}>Already have an account?<Text style={styles.whiteFont} onPress={ ()=> navigate('Login') }> Log In</Text></Text>
+    </View>
+    </TouchableOpacity>
+    </View>
+    </Image>
+    </View>
+);
+}
+}
+
+let styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    bg: {
+        paddingTop: 30,
+        width: null,
+        height: null
+    },
+    headerContainer: {
+        flex: 1,
+    },
+    inputsContainer: {
+        flex: 3,
+        marginTop: 50,
+    },
+    footerContainer: {
+        flex: 1
+    },
+    headerIconView: {
+        marginLeft: 10,
+        backgroundColor: 'transparent'
+    },
+    headerBackButtonView: {
+        width: 20,
+        height: 20,
+    },
+    backButtonIcon: {
+        width: 25,
+        height: 25,
+    },
+    headerTitleView: {
+        backgroundColor: 'transparent',
+        marginTop: 20,
+        marginLeft: 20,
+    },
+    titleViewText: {
+        fontSize: 40,
+        color: '#fff',
+    },
+    inputs: {
+        paddingVertical: 20,
+    },
+    inputContainer: {
+        borderWidth: 1,
+        borderBottomColor: '#CCC',
+        borderColor: 'transparent',
+        flexDirection: 'row',
+        height: 50,
+    },
+    iconContainer: {
+        paddingHorizontal: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputIcon: {
+        width: 20,
+        height: 20,
+    },
+    input: {
+        flex: 1,
+        paddingHorizontal: 10,
+    },
+    signup: {
+        backgroundColor: '#FFF',
+        paddingVertical: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    login: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+    },
+    greyFont: {
+        color: '#D8D8D8'
+    },
+    blackFont: {
+        color: '#000'
+    },
+    whiteFont: {
+        color: '#FFF'
+    }
+});
